fa74f6b72b8b9123129aa93a4eda6a069a795204
Kristopher LeCorgne <klecorgne@gmail.com>
2017-10-15 14:53:53 -0400

    Commit on 10.15.17 including CSV/XCL, CSS, HTML and new Routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,16 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
+  require 'csv'
 
   # GET /products
   # GET /products.json
   def index
     @products = Product.all
+    respond_to do |format|
+      format.html
+      format.csv { send_data @products.to_csv }
+      format.xls # { send_data @products.to_csv(col_sep: "\t") }
+    end
   end
 
   # GET /products/1
@@ -59,6 +65,13 @@ class ProductsController < ApplicationController
       format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
       format.json { head :no_content }
     end
+  end
+  
+  def csv_export
+  end
+
+  def csv_export_all
+    @products = Product.all
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'products/index'
+  root 'products#index'
   resources :products
+  get 'export_all' => 'products#csv_export'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
