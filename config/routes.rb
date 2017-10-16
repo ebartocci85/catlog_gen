068f3c61d4d62a9c9dbd6ac46ae78c001c96ad4b
Kristopher LeCorgne <klecorgne@gmail.com>
2017-10-16 19:54:18 -0400

    Updates to the CSV, Rendering the picture code for the image tag and adding a home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'products/index'
-  root 'products#index'
+  root 'welcome#index'
   resources :products
   get 'export_all' => 'products#csv_export'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
