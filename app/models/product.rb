684993f2f31b52e019b38589896511b64699bbbf
ebartocci85 <emilianofranco@gmail.com>
2017-10-19 20:46:31 -0400

    XLS and XLSX support for catalog generator out in html by emi

--- app/models/product.rb
+++ app/models/product.rb
@@ -15,10 +15,11 @@ class Product < ApplicationRecord
 	  spreadsheet = open_spreadsheet(file)
 	  header = spreadsheet.row(1)
 	  (2..spreadsheet.last_row).each do |i|
-	    row = Hash[[header, spreadsheet.row(i)].transpose]
+
+		  row = Hash[[header, spreadsheet.row(i)].transpose]
 	    product = find_by_id(row["id"]) || new
-	  #  product.attributes = row.to_hash.slice("upc","sku","brand","size", "currency", "retail", 	 
-		product.upc = row.to_hash["UPC"]
+	  #  product.attributes = row.to_hash.slice("upc","sku","brand","size", "currency", "retail",
+		product.upc = row.to_hash["UPC"].to_i
 		product.sku = row.to_hash["SKU"]
 		product.brand = row.to_hash["BRAND"]
 		product.size = row.to_hash["SIZE"]
@@ -41,7 +42,7 @@ class Product < ApplicationRecord
 
 	def self.open_spreadsheet(file)
 	  case File.extname(file.original_filename)
-	  when ".csv" then Csv.new(file.path, nil, :ignore)
+	  when ".csv" then Roo::CSV.new(file.path, nil, :ignore)
 	  when ".xls" then Roo::Excel.new(file.path)
 	  when ".xlsx" then Roo::Excelx.new(file.path)
 	  else raise "Unknown file type: #{file.original_filename}"
